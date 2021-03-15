91486999d9c7fe98d065c9308862a57dc5c8bb6c
Egor Spirin <spirin.egor@mail.ru>
2021-03-15 18:40:41 +0300

    Create setup config

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.0"
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+with open("requirements.txt") as requirements_file:
+    install_requires = [line.strip() for line in requirements_file]
+
+setup_args = dict(
+    name="commode-utils",
+    version=VERSION,
+    description="Set of useful functions and modules for Code Modelling",
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    install_requires=install_requires,
+    license="Apache 2.0",
+    packages=find_packages(),
+    author="Egor Spirin",
+    author_email="spirin.egor@gmail.com",
+    keywords=[],
+    url="https://github.com/SpirinEgor/commode-utils",
+    download_url="https://pypi.org/project/commode-utils/",
+)
+
+if __name__ == "__main__":
+    setup(**setup_args)
