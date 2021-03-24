967b5c227cef5d3e338f39fb892720223d0c33db
Egor Spirin <spirin.egor@mail.ru>
2021-03-24 20:41:31 +0300

    Implement classifier

--- commode_utils/modules/__init__.py
+++ commode_utils/modules/__init__.py
@@ -1,5 +1,6 @@
 from .attention import LuongAttention, LocalAttention
 from .lstm_decoder_step import LSTMDecoderStep
 from .decoder import Decoder
+from .classifier import Classifier
 
-__all__ = ["LuongAttention", "LocalAttention", "LSTMDecoderStep", "Decoder"]
+__all__ = ["LuongAttention", "LocalAttention", "LSTMDecoderStep", "Decoder", "Classifier"]
