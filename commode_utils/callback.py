688a8fe31986a0f18d674ed10280f7d94cfe44c5
Egor Spirin <spirin.egor@mail.ru>
2021-06-22 22:21:58 +0300

    Add split symbol into print epoch callback

--- commode_utils/callback.py
+++ commode_utils/callback.py
@@ -55,18 +55,20 @@ class UploadCheckpointCallback(OldEpochEndCallback):
 class PrintEpochResultCallback(OldEpochEndCallback):
     """Print metrics from specified groups after each epoch."""
 
-    def __init__(self, *groups: str):
+    def __init__(self, *groups: str, split_symbol: str = "/"):
         super().__init__()
         self._groups = groups
+        self._split_symbol = split_symbol
 
     def _old_on_epoch_end(self, trainer: Trainer):
         metrics_to_print: Dict[str, List[str]] = {group: [] for group in self._groups}
         for key, value in trainer.callback_metrics.items():
-            if "/" not in key:
+            if self._split_symbol not in key:
                 continue
-            group, metric = key.split("/")
+            group, metric = key.split(self._split_symbol, 1)
             if group in metrics_to_print:
                 if isinstance(value, torch.Tensor):
                     value = value.item()
                 metrics_to_print[group].append(f"{metric}={round(value, 2)}")
+        metrics_to_print = {k: v for k, v in metrics_to_print.items() if len(v) > 0}
         print_table(metrics_to_print)
