174495539504f73bfa14c9889b55328f7bf06113
Egor Spirin <spirin.egor@mail.ru>
2021-06-25 19:01:20 +0300

    Reimplemented callbacks

--- commode_utils/callback.py
+++ commode_utils/callback.py
@@ -1,6 +1,6 @@
 from abc import abstractmethod
 from os.path import split, join
-from typing import Dict, List, Any, Optional
+from typing import Dict, List, Optional, Union
 
 import torch
 from pytorch_lightning import Callback, Trainer, LightningModule
@@ -9,42 +9,14 @@ from pytorch_lightning.loggers import WandbLogger
 from commode_utils.common import print_table
 
 
-class OldEpochEndCallback(Callback):
-    """Implement behaviour of hooks like it was in <=1.1.7 version
-    More details:
-    https://github.com/PyTorchLightning/pytorch-lightning/issues/5917
-    """
-
-    def __init__(self):
-        self._is_training = False
-
-    def on_train_start(self, trainer: Trainer, pl_module: LightningModule):
-        self._is_training = True
-
-    def on_train_end(self, trainer: Trainer, pl_module: LightningModule):
-        self._is_training = False
-
-    def on_train_epoch_end(self, trainer: Trainer, pl_module: LightningModule, unused: Optional[bool] = None):
-        if trainer.val_dataloaders is None:
-            self._old_on_epoch_end(trainer)
-
-    def on_validation_epoch_end(self, trainer: Trainer, pl_module: LightningModule):
-        if self._is_training:
-            self._old_on_epoch_end(trainer)
-
-    @abstractmethod
-    def _old_on_epoch_end(self, trainer: Trainer):
-        raise NotImplementedError()
-
-
-class UploadCheckpointCallback(OldEpochEndCallback):
-    """Forced upload saved checkpoint after every epoch. Works only for wandb."""
+class UploadCheckpointCallback(Callback):
+    """Upload checkpoints after every validation epoch. Works only for wandb."""
 
     def __init__(self, checkpoint_dir: str):
         super().__init__()
         self._checkpoint_dir = checkpoint_dir
 
-    def _old_on_epoch_end(self, trainer: Trainer):
+    def on_validation_epoch_end(self, trainer: Trainer, pl_module: LightningModule):
         logger = trainer.logger
         if isinstance(logger, WandbLogger):
             experiment = logger.experiment
@@ -52,23 +24,43 @@ class UploadCheckpointCallback(OldEpochEndCallback):
             experiment.save(join(self._checkpoint_dir, "*.ckpt"), base_path=root_dir)
 
 
-class PrintEpochResultCallback(OldEpochEndCallback):
-    """Print metrics from specified groups after each epoch."""
+class PrintEpochResultCallback(Callback):
+    """Print metrics to stdout after each specified epoch."""
 
-    def __init__(self, *groups: str, split_symbol: str = "/"):
+    def __init__(
+        self, after_train: bool = True, after_validation: bool = True, after_test: bool = True, split_symbol: str = "/"
+    ):
         super().__init__()
-        self._groups = groups
-        self._split_symbol = split_symbol
+        self.__after_train = after_train
+        self.__after_validation = after_validation
+        self.__after_test = after_test
+        self.__split_symbol = split_symbol
 
-    def _old_on_epoch_end(self, trainer: Trainer):
-        metrics_to_print: Dict[str, List[str]] = {group: [] for group in self._groups}
-        for key, value in trainer.callback_metrics.items():
-            if self._split_symbol not in key:
+    def _print_metric(self, logged_metrics: Dict[str, Union[float, torch.Tensor]], step: str):
+        metrics_to_print = {step: []}
+        for key, value in logged_metrics.items():
+            if self.__split_symbol not in key:
                 continue
-            group, metric = key.split(self._split_symbol, 1)
-            if group in metrics_to_print:
-                if isinstance(value, torch.Tensor):
-                    value = value.item()
-                metrics_to_print[group].append(f"{metric}={round(value, 2)}")
-        metrics_to_print = {k: v for k, v in metrics_to_print.items() if len(v) > 0}
-        print_table(metrics_to_print)
+            group, metric = key.split(self.__split_symbol, 1)
+            if group != step:
+                continue
+            if isinstance(value, torch.Tensor):
+                value = value.item()
+            metrics_to_print[group].append(f"{metric} = {round(value, 2)}")
+        if len(metrics_to_print[step]) > 0:
+            print_table(metrics_to_print)
+
+    def on_train_epoch_end(self, trainer: Trainer, pl_module: LightningModule, unused: Optional[bool] = None):
+        if not self.__after_train:
+            return
+        self._print_metric(trainer.logged_metrics, "train")
+
+    def on_validation_epoch_end(self, trainer: Trainer, pl_module: LightningModule):
+        if not self.__after_validation:
+            return
+        self._print_metric(trainer.logged_metrics, "val")
+
+    def on_test_epoch_end(self, trainer: Trainer, pl_module: LightningModule):
+        if not self.__after_test:
+            return
+        self._print_metric(trainer.logged_metrics, "test")
