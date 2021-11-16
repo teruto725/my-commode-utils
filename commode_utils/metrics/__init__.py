f814d887ff54b9ddfabdae4c06283d06b58da7b4
Egor Spirin <spirin.egor@mail.ru>
2021-11-16 12:02:22 +0300

    Update version, decompose callback into separate package

--- commode_utils/metrics/__init__.py
+++ commode_utils/metrics/__init__.py
@@ -1,4 +1,5 @@
 from .classification import ClassificationMetrics
 from .sequential_f1 import SequentialF1Score
+from .chrF import ChrF
 
-__all__ = ["ClassificationMetrics", "SequentialF1Score"]
+__all__ = ["ClassificationMetrics", "SequentialF1Score", "ChrF"]
